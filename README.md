ca01caba679a6f3311649a943d0d01163904e0bf
HenriqueFelixDev <henriquefelixdev@gmail.com>
2023-07-22 08:09:38 -0300

    feat: start project

---
+++ README.md
@@ -0,0 +1,3 @@
+# pomodoro_prodigy
+
+Aplicação de gestão de tempo utilizando a técnica pomodoro
---
+++ android/app/src/main/kotlin/br/dev/henriquefelix/pomodoro_prodigy/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.dev.henriquefelix.pomodoro_prodigy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
